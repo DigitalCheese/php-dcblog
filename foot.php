84eff6fc72fcaba5fdc190c36adf26e0a2f4651c
Digital Cheese <digitalcheese@att.net>
2024-01-25 04:41:06 +0000

    example uwu

--- foot.php
+++ foot.php
@@ -2,22 +2,23 @@
 echo '</div>';
 echo '<div class="footer">
 <div class="f1"><h2>About</h2>
-2021-2023<br>
+2021-2024<br>
 Digital Cheese<br>
 All Rights Reserved
+<h2>Mirrors</h2>
+<a href="">TOR</a><br>
+<a href="https://uwu.i2p/">I2P</a> (<a href="">b32</a>)
 </div><br>
 <div class="f2"><h2>Contact</h2>
 <a href="mailto:dc@tfwno.gf">Email</a> (<a href="/m/pgp.asc">PGP</a>)<br>
 <a href="https://3dspaint.com/member?id=150961">3DSPaint</a><br>
 <a href="https://forum.agoraroad.com/index.php?members/6215/">Agora Road</a><br>
-<a href="https://social.venith.net/u/DigitalCheese">Fediverse</a><br>
 <a href="https://codeberg.org/DigitalCheese">Codeberg</a>
-<h2>Extra</h2>
-<a href="">TOR</a><br>
-<a href="https://uwu.i2p/">I2P</a> (<a href="">b32</a>)<br>
-<a href="/rss.xml">RSS Feed</a>
+<h2>RSS Feeds</h2>
+<a href="/w.xml">Writings</a>
 </div><br>
 <div class="f3"><h2>Affiliates</h2>
-<a href="https://venith.net/">Venith</a> (<a href="">TOR</a>,<a href="https://iwu.i2p/">I2P</a>)</div>
+<a href="https://venith.net/">Venith</a> (<a href="">TOR</a>,<a href="https://iwu.i2p/">I2P</a>)<br>
+<a href="https://funfacts.top/">Fun Facts Community</a></div>
 </div>';
 ?>
--- w/index.php
+++ w/index.php
@@ -1,9 +1,18 @@
 <?php
 
-<h2 id=b>Books</h2>
-<a href="/w/cyber-miso_2024.pdf">Cyber Miso</a> (2024)
-<h2 id=a>Articles</h2>
-<a href="/w/?a=mpga">Make Paint Great Again</a> (2023)
-<h2 id=s>Statuses</h2>
+include '/head.php';
+
+echo '<h1 id=a>Articles</h1>
+<a href="/w/?a=mpga">Make Paint Great Again</a>
+<h1 id=s>Statuses</h1>
+<h2>2024</h2>
+None...
+<h2>2023</h2>
+None...
+<h1 id=b>Books</h1>
+<h2>2024</h2>
+Example Book | <a href="/m/books/example-book_2024.pdf">PDF</a>PDF</a>';
+
+include '/foot.php';
 
 ?>
